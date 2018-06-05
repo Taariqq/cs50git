ee87974e80d4b77ad70a6785fa123e6bfe90f819
Tariq Qureshi <taariqq@gmail.com>
2018-06-06 01:30:31 +0500

    redone

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 34;
+    int y = 50;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    
+    // swapping...
+
+    
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
